88563769539bdd11f11d4246b38d6a4e4b071755
Dany <dnyanesh2442@gmail.com>
2024-12-19 20:25:33 +0530

    colors file Added

---
+++ src/constants/color.js
@@ -0,0 +1,7 @@
+export const color = {
+    background : "#09127" ,
+    textPrimary : "#EAF0FF" ,
+    textSecondary : "#A5C0FF" ,
+    iconPrimary : "#EAF0FF" ,
+    iconSecondary : "#8996B8" ,
+}
